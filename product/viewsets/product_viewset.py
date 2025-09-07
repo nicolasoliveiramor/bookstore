455d570ba280fa42c9d3eb1ca82874ad1596d090
Nicolas Oliveira Mor <nicolasoliveiramor05@gmail.com>
2025-09-07 20:46:33 -0300

    Feature: Adding docker-compose/Makefile

--- product/viewsets/product_viewset.py
+++ product/viewsets/product_viewset.py
@@ -4,6 +4,7 @@ from rest_framework.permissions import AllowAny
 from product.models import Product
 from product.serializers.product_serializer import ProductSerializer
 
+
 class ProductViewSet(ModelViewSet):
     permission_classes = [AllowAny]
     serializer_class = ProductSerializer
