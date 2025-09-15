9ec29e43c32bee5cff8daca2f8ae92b27c0547bc
Nicolas Oliveira Mor <nicolasoliveiramor05@gmail.com>
2025-09-14 22:09:19 -0300

    feat: work in progress

--- product/viewsets/product_viewset.py
+++ product/viewsets/product_viewset.py
@@ -1,12 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import AllowAny
 
 from product.models import Product
 from product.serializers.product_serializer import ProductSerializer
 
 
 class ProductViewSet(ModelViewSet):
-    permission_classes = [AllowAny]
     serializer_class = ProductSerializer
 
     def get_queryset(self):
